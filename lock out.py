22d0a05fded9dceda03d34118f8fba4a0268aad8
Eric Mailman <emailman@dtcc.edu>
2018-11-18 16:20:34 -0500

    Added "lock out" program with comments

--- lock out.py
+++ lock out.py
@@ -4,6 +4,7 @@ from time import sleep
 sensor = physense_emu.Sensor()
 
 while True:
+    # Show green if unlocked, red if locked
     lock_status = sensor.input('light')
     if lock_status == 'on':
         sensor.output('gled', 'on')
@@ -12,6 +13,7 @@ while True:
         sensor.output('gled', 'off')
         sensor.output('rled', 'on')
 
+    # Run a cycle only if unlocked
     if sensor.input('Button_4') == 'pressed' and lock_status == 'on':
         for i in range(5):
             sensor.output('bled', 'on')
